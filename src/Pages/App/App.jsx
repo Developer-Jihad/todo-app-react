afee069dc17c6b00e57b927b265c2460141df502
Developer-Jihad <jihad.developer.bd@gmail.com>
2023-11-25 10:40:02 +0600

    [Again Done]

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Todos from "./../Todos/Todos";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Todos />
+      </div>
+    </>
+  );
+}
+
+export default App;
